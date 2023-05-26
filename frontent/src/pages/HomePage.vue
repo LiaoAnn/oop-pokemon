76c0a00e9be171bcdfd63ac9fe22e9849017ad68
LiaoAnn <1050123tom@gmail.com>
2023-05-26 23:16:15 +0800

    feat: added font-family; adjusted home-page;

--- frontent/src/pages/HomePage.vue
+++ frontent/src/pages/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <n-layout-content>
     <div class="w:75% margin:2em|auto">
-      <div class="grid-cols:2 h:100%">
-        <div class="p:2em">
-          <div class="font:3em mt:.75em font-weight:900">Project 3 | Pokémon</div>
-          <div class="font:1.2em mt:.5em line-height:2em font-weight:500 f:#aaa">
+      <div class="grid-cols:2 h:100% align-items:center">
+        <div>
+          <div class="font:3em font-weight:900">Project 3 | Pokémon</div>
+          <div class="font:1.2em mt:.5em line-height:1.75em font-weight:500 f:#aaa">
             歡迎你進入寶可夢的世界，<br />
             在這裡你將和全球頂尖的訓練家們展開刺激的對決。<br />
             每一次的勝利，<br />
@@ -12,11 +12,17 @@
             你，準備好證明自己，<br />
             成為最強大的寶可夢訓練家了嗎？
           </div>
-          <div class="mt:6em flex gap:8px">
-            <n-button class="p:1em|2em font:2em font-weight:500" type="primary">
-              <n-icon class="mr:8px" size="32px">
-                <PokemonBallIcon />
-              </n-icon>
+          <div class="mt:6em flex gap:16px">
+            <n-button
+              class="p:1em|2em font:2em font-weight:500"
+              type="primary"
+              @click="$router.push({ name: 'Game' })"
+            >
+              <template #icon>
+                <n-icon class="mr:16px" size="32px">
+                  <PokemonBallIcon />
+                </n-icon>
+              </template>
               Play
             </n-button>
             <n-button
@@ -37,7 +43,7 @@
           </div>
         </div>
         <div
-          :class="`bg:url('${HomeImage}') height:600px background-size:160% bg:no-repeat background-position:0%|50%`"
+          :class="`bg:url('${HomeImage}') height:600px background-size:125% bg:no-repeat background-position:0%|50%`"
         ></div>
       </div>
     </div>
