8b19bed7dc188dc8f70dcf3915c04e7009ef254b
LiaoAnn <1050123tom@gmail.com>
2023-05-26 13:54:20 +0800

    feat: finished site-footer

--- frontent/src/layouts/MainLayout.vue
+++ frontent/src/layouts/MainLayout.vue
@@ -2,11 +2,13 @@
   <n-layout>
     <SiteHeader />
     <router-view />
+    <SiteFooter />
   </n-layout>
 </template>
 
 <script setup lang="ts">
 import { NLayout } from 'naive-ui';
 
+import SiteFooter from '@/components/SiteFooter.vue';
 import SiteHeader from '@/components/SiteHeader.vue';
 </script>
