b4ae8127a4bafb26c7ec5a7d294b6952827af2ee
LiaoAnn <1050123tom@gmail.com>
2023-05-26 23:56:39 +0800

    feat: added routing progress-bar

--- frontent/src/router/index.ts
+++ frontent/src/router/index.ts
@@ -1,10 +1,37 @@
+import { createDiscreteApi } from 'naive-ui';
 import { createRouter, createWebHistory } from 'vue-router';
+
+import useTheme from '@/common/useTheme';
 
 import routes from './routes';
 
+const { primaryColor } = useTheme();
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+});
+const { loadingBar } = createDiscreteApi(['loadingBar'], {
+  loadingBarProviderProps: {
+    loadingBarStyle: {
+      loading: `background-color: ${primaryColor};`
+    }
+  }
+});
+router.beforeEach(function (to, from, next) {
+  if (!from || to.path !== from.path) {
+    if (loadingBar) {
+      loadingBar.start();
+    }
+  }
+  next();
+});
+
+router.afterEach(function (to, from) {
+  if (!from || to.path !== from.path) {
+    if (loadingBar) {
+      loadingBar.finish();
+    }
+  }
 });
 
 export default router;
