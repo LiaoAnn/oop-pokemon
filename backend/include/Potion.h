b6f0c3ffa488aeecf8f6095576407c1c514c9362
binga288 <67582826+binga288@users.noreply.github.com>
2023-06-08 20:16:36 +0800

    feat: Added Potion class

---
+++ backend/include/Potion.h
@@ -0,0 +1,59 @@
+﻿/***********************************************************************
+ * File: Potion.h
+ * Author: BING-JIA TAN (B11115001)
+ * Create Date: 2023-06-08
+ * Editor: BING-JIA TAN (B11115001)
+ * Update Date: 2023-06-08
+ * Description: Definition of class Potion
+************************************************************************/
+#pragma once
+
+#include "Item.h"
+#include "Pokemon.h"
+
+// Potions can affect the HP of the current Pokémon, but they cannot be used on a copied Pokémon.
+class Potion : public Item
+{
+public:
+	Potion();
+	Potion(string name, int points);
+	~Potion();
+
+	virtual void usePotion() = 0;
+};
+
+class NormalPotion : public Potion
+{
+public:
+	NormalPotion();
+	~NormalPotion();
+
+	void usePotion(Pokemon& pokemon) override;
+};
+
+class SuperPotion : public Potion
+{
+public:
+	SuperPotion();
+	~SuperPotion();
+
+	void usePotion(Pokemon& pokemon) override;
+};
+
+class HyperPotion : public Potion
+{
+public:
+	HyperPotion();
+	~HyperPotion();
+
+	void usePotion(Pokemon& pokemon) override;
+};
+
+class MaxPotion : public Potion
+{
+public:
+	MaxPotion();
+	~MaxPotion();
+
+	void usePotion(Pokemon& pokemon) override;
+};
---
+++ backend/src/Potion.cpp
@@ -0,0 +1,139 @@
+﻿/***********************************************************************
+ * File: Potion.cpp
+ * Author: BING-JIA TAN (B11115001)
+ * Create Date: 2023-06-08
+ * Editor: BING-JIA TAN (B11115001)
+ * Update Date: 2023-06-08
+ * Description: Implementation of class Potion
+************************************************************************/
+
+#include "Potion.h"
+
+/**
+ * Intent: Default constructor
+ * Pre: None
+ * Post: A Potion object is created with name "Potion" and points 0
+ */
+Potion::Potion() : Item("Potion", 0)
+{}
+
+/**
+ * Intent: Constructor with parameters
+ * Pre: None
+ * Post: A Potion object is created with name and points
+ */
+Potion::Potion(string name, int points) : Item(name, points)
+{}
+
+/**
+ * Intent: Destructor
+ * Pre: None
+ * Post: None
+ */
+Potion::~Potion()
+{}
+
+/**
+ * Intent: Normal potion constructor
+ * Pre: None
+ * Post: A NormalPotion object is created with name "Normal Potion" and points 20
+ */
+NormalPotion::NormalPotion() : Potion("Potion", 20)
+{}
+
+/**
+ * Intent: Destructor
+ * Pre: None
+ * Post: None
+ */
+NormalPotion::~NormalPotion()
+{}
+
+/**
+ * Intent: Use the potion on the pokemon
+ * Pre: None
+ * Post: The pokemon's HP is increased by 20
+ */
+void NormalPotion::usePotion(Pokemon& pokemon)
+{
+	pokemon.healHp(20);
+}
+
+/**
+ * Intent: Hyper potion constructor
+ * Pre: None
+ * Post: A HyperPotion object is created with name "Hyper Potion" and points 200
+ */
+SuperPotion::SuperPotion() : Potion("Super Potion", 50)
+{}
+
+/**
+ * Intent: Destructor
+ * Pre: None
+ * Post: None
+ */
+SuperPotion::~SuperPotion()
+{}
+
+/**
+ * Intent: Use the potion on the pokemon
+ * Pre: None
+ * Post: The pokemon's HP is increased by 50
+ */
+void SuperPotion::usePotion(Pokemon& pokemon)
+{
+	pokemon.healHp(50);
+}
+
+/**
+ * Intent: Hyper potion constructor
+ * Pre: None
+ * Post: A HyperPotion object is created with name "Hyper Potion" and points 200
+ */
+HyperPotion::HyperPotion() : Potion("Hyper Potion", 200)
+{}
+
+/**
+ * Intent: Destructor
+ * Pre: None
+ * Post: None
+ */
+HyperPotion::~HyperPotion()
+{}
+
+/**
+ * Intent: Use the potion on the pokemon
+ * Pre: None
+ * Post: The pokemon's HP is increased by 200
+ */
+void HyperPotion::usePotion(Pokemon& pokemon)
+{
+	pokemon.healHp(200);
+}
+
+/**
+ * Intent: Max potion constructor
+ * Pre: None
+ * Post: A MaxPotion object is created with name "Max Potion" and points 999
+ */
+MaxPotion::MaxPotion() : Potion("Max Potion", 999)
+{}
+
+/**
+ * Intent: Destructor
+ * Pre: None
+ * Post: None
+ */
+MaxPotion::~MaxPotion()
+{}
+
+/**
+ * Intent: Use the potion on the pokemon
+ * Pre: None
+ * Post: The pokemon's HP is increased to its maximum
+ */
+void MaxPotion::usePotion(Pokemon& pokemon)
+{
+	int mapHp = pokemon.getMaxHp();
+	pokemon.healHp(mapHp);
+}
