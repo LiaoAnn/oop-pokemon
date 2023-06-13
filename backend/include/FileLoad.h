338ea0352502c25bec0f3e2b1f62572f7b3cb82b
binga288 <67582826+binga288@users.noreply.github.com>
2023-06-13 21:33:59 +0800

    refactor: isFileLoad to public

--- backend/include/FileLoad.h
+++ backend/include/FileLoad.h
@@ -22,22 +22,11 @@ class FileLoad {
 private:
 	string caseFilename;
 	string MonsterLibName, MoveLibName, GameDataName;
-	//int hp; // health points
-	//int attack; // attack points
-	//int defence; // defence points
-	//int spAttack; // special attack points
-	//int spDefence; // special defence points
-	//int speed;
-	//map<string, int> TypeMap = { {"Normal", 0}, {"Fire", 1}, {"Water", 2}, {"Electric", 3}, {"Grass", 4}, {"Ice", 5}, {"Fighting", 6}, {"Poison", 7}, {"Ground", 8}, {"Flying", 9}, {"Psychic", 10}, {"Bug", 11}, {"Rock", 12}, {"Ghost", 13}, {"Dragon", 14}, {"Dark", 15}, {"Steel", 16}, {"Fairy", 17} };
-
-	/*set<int> typelist;
-	vector<Pokemon> Monster;
-	vector<Pokemon>player1, player2;*/
-	//vector<Move> MoveLib;
+public:
 	bool isLoadGameData = false;
 	bool isLoadMonsterLib = false;
 	bool isLoadMoveLib = false;
-public:
+
 	// default constructor
 	FileLoad();
 	FileLoad(string file);
