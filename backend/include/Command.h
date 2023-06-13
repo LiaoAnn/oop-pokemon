aecfe6bb4685a1028cbe8731bef0b647994e254e
binga288 <67582826+binga288@users.noreply.github.com>
2023-06-14 02:42:20 +0800

    feat: Added Command for API result

---
+++ backend/include/Command.h
@@ -0,0 +1,20 @@
+﻿/***********************************************************************
+ * File: Command.h
+ * Author: BING-JIA TAN (B11115001)
+ * Create Date: 2023-06-14
+ * Editor: BING-JIA TAN (B11115001)
+ * Update Date: 2023-06-14
+ * Description: Header file for Command
+************************************************************************/
+
+#include "json.h"
+#include "Pokemon.h"
+#include "Skill.h"
+
+// attach command result to json
+json attactCommandResult(
+	Pokemon& firstPokemon, Pokemon& secondPokemon,
+	Skill& firstSkill, Skill& secondSkill,
+	int firstDamage, int secondDamage,
+	int firstPlayerIndex
+);
---
+++ backend/src/Command.cpp
@@ -0,0 +1,52 @@
+﻿/***********************************************************************
+ * File: Command.cpp
+ * Author: BING-JIA TAN (B11115001)
+ * Create Date: 2023-06-14
+ * Editor: BING-JIA TAN (B11115001)
+ * Update Date: 2023-06-14
+ * Description: Implementation for Command
+************************************************************************/
+
+#include "Command.h"
+
+
+/**
+ * Intent: Return the result of attack command
+ * Pre:
+ * Post:
+ */
+json attactCommandResult
+(
+	Pokemon& firstPokemon, Pokemon& secondPokemon,
+	Skill& firstSkill, Skill& secondSkill,
+	int firstDamage, int secondDamage,
+	int firstPlayerIndex
+)
+{
+	json result = json();
+	json myMonsterJson = (firstPlayerIndex == 0)
+		? firstPokemon.toJson() : secondPokemon.toJson();
+	json otherMonsterJson = (firstPlayerIndex == 0)
+		? secondPokemon.toJson() : firstPokemon.toJson();
+	json myMoveJson = (firstPlayerIndex == 0)
+		? firstSkill.toJson() : secondSkill.toJson();
+	json otherMoveJson = (firstPlayerIndex == 0)
+		? secondSkill.toJson() : firstSkill.toJson();
+
+	int myDamage = (firstPlayerIndex == 0) ? firstDamage : secondDamage;
+	int otherDamage = (firstPlayerIndex == 0) ? secondDamage : firstDamage;
+
+	result["type"] = "attack";
+	result["success"] = true;
+	result["attackFirst"] = (firstPlayerIndex == 0) ? "me" : "other";
+
+	result["myDamage"] = (firstPlayerIndex == 0) ? myDamage : otherDamage;
+	result["myMove"] = myMoveJson;
+	result["myMonster"] = myMonsterJson;
+
+	result["otherDamage"] = (firstPlayerIndex == 0) ? myDamage : otherDamage;
+	result["otherMove"] = otherMoveJson;
+	result["otherMonster"] = otherMonsterJson;
+
+	return result;
+}
