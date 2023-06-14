2552fe5ae34e6421ddb83ebccc1c98d74f227b03
binga288 <67582826+binga288@users.noreply.github.com>
2023-06-14 21:27:45 +0800

    Release Version

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -18,23 +18,6 @@
 
 int main()
 {
-	/*string filename;
-	filename = "../../../src/case.txt";
-	FileLoad filetest(filename);
-	filetest.openFile();
-	filetest.PokemonLibfile();
-	filetest.Movesfile();
-	filetest.Gamedata();*/
-
-	try
-	{
-		file.loadCaseFile("case4.txt");//file case.txt
-	}
-	catch (string& e)
-	{
-		game << e;
-	}
-
 	webServer = new WebServer(WEBROOT, WEBPORT);
 	webSocketServer = new WebSocketServer();
 	thread web(webServerThread);
