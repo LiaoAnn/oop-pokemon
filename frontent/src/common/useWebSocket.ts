22104f9bdb2fd251e7b9dfcb392cf770a8b49487
LiaoAnn <1050123tom@gmail.com>
2023-06-14 10:54:29 +0800

    feat: added send-msg-type and receive-msg-type; added receive-msg props

--- frontent/src/common/useWebSocket.ts
+++ frontent/src/common/useWebSocket.ts
@@ -1,17 +1,27 @@
 export enum SendMsgType {
   Init = 'init',
-  LoadFile = 'loadFile'
+  LoadMonster = 'load_monster',
+  LoadMove = 'load_move',
+  LoadGameData = 'load_game_data',
+  LoadCase = 'load_case'
 }
 
-export enum ReceiveMsgType {}
+export enum ReceiveMsgType {
+  Init = 'init',
+  LoadCase = 'load_case'
+}
 
 export interface SendMsg {
   type: SendMsgType;
+  file?: string;
 }
 
 export interface ReceiveMsg {
   type: ReceiveMsgType;
-  success?: boolean;
+  success: boolean;
+  battle_log: string[];
+  myMonsters: string[];
+  otherMonsters: string[];
 }
 
 export function useWebSocket(onMsgCallback: (e: MessageEvent) => void) {
